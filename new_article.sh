37767c542b9fc914da61808b612ed4c0871ee40a
jarlin8 <jarlinwei@gmail.com>
2022-04-14 01:13:47 +0800

    ggxv

--- new_article.sh
+++
@@ -1,3 +0,0 @@
-echo -n  "请输入文件名 ->"
-read  filename
-touch  ./_posts/$(date "+%Y-%m-%d-%H-%M-%S-$filename.md")
--- tagit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-git add .
-git commit -m "ok"
-#git tag $1
-git push
-#git push --tags
